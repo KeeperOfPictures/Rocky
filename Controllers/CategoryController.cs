f98face3ac276646dca210f8c769584eeb3c85dd
Pyotr Smirnov <Petya_smit@mail.ru>
2022-06-28 22:00:50 +0300

    Добавьте файлы проекта.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky_Tile.Data;
+using Rocky_Tile.Models;
+using System.Collections.Generic;
+
+namespace Rocky_Tile.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rocky_Tile.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
